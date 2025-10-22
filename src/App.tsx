a0eb69a7dce99021c7644578c61c50da7d489c2d
roslynsk <rmshawkelly@gmail.com>
2025-10-22 19:29:03 +0100

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <div className="app">
         <Navbar />
 
